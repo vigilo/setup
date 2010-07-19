ab67b507e755e9f54a136038e5028e11e083e019
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-07-19 10:22:13 +0000

    Correction du script de VigiConf, affichage du mdp de Nagios
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@3829 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/200-conf-vigiconf/run.sh
+++ modules/200-conf-vigiconf/run.sh
@@ -9,13 +9,28 @@ if [ ! -d /etc/vigilo/vigiconf/conf.d ]; then
     exit 0
 fi
 
+[ -f /etc/vigilo/vigiconf/conf.d/hosts/localhost.xml ] || \
+    cp -p localhost.xml /etc/vigilo/vigiconf/conf.d/hosts/
+
+# sudo
+grep -qs ^vigiconf /etc/sudoers || echo 'vigiconf ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+
+# serveur de supervision
+sed -i -e 's/supserver.example.com/localhost/' /etc/vigilo/vigiconf/conf.d/general/appgroups-servers.py
+
+# Désactivation de CorrTrap
+if [ ! -f /etc/vigilo/vigiconf/conf.d/general/apps.py.orig ]; then
+    cwd=`pwd`
+    pushd /etc/vigilo/vigiconf
+    patch -b -p4 -N < $cwd/vigiconf.patch || :
+    popd
+fi
+
+# SVN
 if [ -d /etc/vigilo/vigiconf/conf.d/.svn ]; then
     echo "Le répertoire de configuration de Vigiconf est déjà sous SVN, on passe à la suite..."
     exit 0
 fi
-
-[ -d /etc/vigilo/vigiconf/conf.d/hosts/localhost.xml ] || \
-    cp -p localhost.xml /etc/vigilo/vigiconf/conf.d/hosts/
 
 if [[ "$svnurl" =~ ^file:// ]]; then
     if ! which svnadmin >/dev/null 2>&1; then
@@ -38,12 +53,6 @@ svn propset svn:keywords Rev conf.d/general/apps.py
 chown vigiconf:vigiconf -R conf.d
 popd
 
-# sudo
-grep -qs ^vigiconf /etc/sudoers || echo 'vigiconf ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
-
-# serveur de supervision
-sed -i -e 's/supserver.example.com/localhost/' /etc/vigilo/vigiconf/conf.d/general/appgroups-servers.py
-
 # settings.ini
 if [ ! -f /etc/vigilo/vigiconf/conf.d/settings.ini.orig ]; then
     sed -i -e 's,^\(svnrepository[[:space:]]*=[[:space:]]*\).*,\1'$svnurl',' \
@@ -51,13 +60,5 @@ if [ ! -f /etc/vigilo/vigiconf/conf.d/settings.ini.orig ]; then
 fi
 
 # permissions
-chown vigiconf: -R /etc/vigilo/vigiconf/conf.d /etc/vigilo/vigiconf/settings.ini
-
-# Désactivation de CorrTrap
-if [ ! -f /etc/vigilo/vigiconf/conf.d/general/apps.py.orig ]; then
-    cwd=`pwd`
-    pushd /etc/vigilo/vigiconf
-    patch -b -p4 -N < $cwd/vigiconf.patch || :
-    popd
-fi
+chown vigiconf: /etc/vigilo/vigiconf/conf.d /etc/vigilo/vigiconf/settings.ini
 
