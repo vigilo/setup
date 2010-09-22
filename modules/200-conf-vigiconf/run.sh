9b5ce037fa37141dc61bb29c4b434e8c4af26a6b
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2010-09-22 16:55:45 +0000

    correction
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4540 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/200-conf-vigiconf/run.sh
+++ modules/200-conf-vigiconf/run.sh
@@ -24,14 +24,6 @@ fi
 # serveur de supervision
 sed -i -e 's/supserver.example.com/localhost/' /etc/vigilo/vigiconf/conf.d.example/general/appgroups-servers.py
 
-# Désactivation de CorrTrap
-if [ ! -f /etc/vigilo/vigiconf/conf.d/general/apps.py.orig ]; then
-    cwd=`pwd`
-    pushd /etc/vigilo/vigiconf/conf.d.example
-    patch -b -p5 -N < $cwd/vigiconf.patch || :
-    popd
-fi
-
 # SVN
 if [ -d /etc/vigilo/vigiconf/conf.d/.svn ]; then
     echo "Le répertoire de configuration de Vigiconf est déjà sous SVN, on passe à la suite..."
