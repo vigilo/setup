1d66563e68a208de23654d87d28b745db7126809
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-09-23 10:18:55 +0000

    Validation de Nagios
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4558 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/100-conf-nagios/run.sh
+++ modules/100-conf-nagios/run.sh
@@ -3,9 +3,9 @@
 echo "Configuration de Nagios"
 
 [ -f /etc/nagios/nagios.cfg.orig ] || patch -N -b /etc/nagios/nagios.cfg < nagios.patch
-if [ ! -f /etc/nagios/nagios-test.cfg ]; then
-    cp -f /etc/nagios/nagios.cfg /etc/nagios/nagios-test.cfg
-    patch -N -b /etc/nagios/nagios-test.cfg < nagios-test.patch
-fi
 [ -f /etc/nagios/vigilo.cfg ] || cp nagios-vigilo.cfg /etc/nagios/vigilo.cfg
 
+# Si la machine VigiConf a aussi un Nagios installé, il faut l'ajouter au
+# groupe vigiconf pour qu'il puisse valider la configuration (qui se trouve
+# dans /var/lib/vigiconf, accessible uniquement au groupe vigiconf.
+getent group vigiconf > /dev/null && useradd -a -G vigiconf nagios
