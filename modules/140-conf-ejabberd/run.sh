ce27cbd2c8aa510c246b079f882e699cddcaf3a6
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2010-09-23 10:44:57 +0000

    correction du patch
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4559 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/140-conf-ejabberd/run.sh
+++ modules/140-conf-ejabberd/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f /etc/ejabberd/ejabberd.cfg.orig ]; then
+if [ ! -f /etc/ejabberd/ejabberd.cfg.orig ]; then
     echo "Patch /etc/ejabberd/ejabberd.cfg"
     patch -N -b /etc/ejabberd/ejabberd.cfg < ejabberd.cfg.patch
     chown root:ejabberd /etc/ejabberd/ejabberd.cfg
