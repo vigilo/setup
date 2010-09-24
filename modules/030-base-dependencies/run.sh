bb6d56b65100b1d86cb2c0d80d65e506659d7a95
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-09-24 11:14:55 +0000

    Rum a été mis à jour
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4589 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/030-base-dependencies/run.sh
+++ modules/030-base-dependencies/run.sh
@@ -21,8 +21,4 @@ urpmi   postgresql8.3-server \
 
 # Besoin de sqlalchemy >= 0.5
 rpm -Uvh $VIGILO_DEPS_REPO/python-sqlalchemy-0.5.6-1mdvmes2009.0.noarch.rpm || :
-# Besoin de python-webob < 0.9.8 pour rum
-rpm -Uvh --oldpackage $VIGILO_DEPS_REPO/python-webob-0.9.6.1-1mdvmes2009.0.noarch.rpm \
-                      $VIGILO_DEPS_REPO/python-repoze.what-quickstart-1.0-1mdvmes2009.0.noarch.rpm \
-                      $VIGILO_DEPS_REPO/python-repoze.who-friendlyform-1.0-b3mdvmes2009.0.noarch.rpm ||:
 
