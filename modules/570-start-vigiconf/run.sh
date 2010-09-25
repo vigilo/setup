3844a219e37f6b5bf406b53162c48e6288e4932d
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2010-09-25 11:50:14 +0000

    correction cas du premier lancement de vigiconf deploy qui ne démarrait pas le connector de métro
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4641 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/570-start-vigiconf/run.sh
+++ modules/570-start-vigiconf/run.sh
@@ -3,3 +3,5 @@
 echo "Lancement de vigiconf"
 vigiconf deploy
 
+# start vigilo-connector-metro if necessary (hook for first attempt of vigiconf deploy...)
+/etc/init.d/vigilo-connector-metro status &> /dev/null || /etc/init.d/vigilo-connector-metro start
