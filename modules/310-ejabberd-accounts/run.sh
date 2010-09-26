9d18c279bd56e12d47132ced7ffe12fb6cfa515c
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2010-09-26 17:31:51 +0000

    correction pour differenciation serveur
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4660 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/310-ejabberd-accounts/run.sh
+++ modules/310-ejabberd-accounts/run.sh
@@ -5,6 +5,8 @@ service ejabberd status &> /dev/null  || service ejabberd start
 sleep 2
 for connector in connector-nagios connector-metro connector-vigiboard connector-diode correlator; do
     password=`awk '/^password/ {print $3}' /etc/vigilo/$connector/settings.ini`
-    su ejabberd -c 'ejabberdctl --node ejabberd@localhost register '$connector' localhost '$password || :
+    # case where a server dont have all the connector.
+    [ -z "$password" ] && password=$connector
+    su ejabberd -c "ejabberdctl --node ejabberd@localhost register '$connector' localhost '$password'" || :
 done
 
