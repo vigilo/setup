0510713b290ea1c9b32f19d40ba83b0badd87d44
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2010-09-26 14:06:58 +0000

    correction affichage
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4653 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/310-ejabberd-accounts/run.sh
+++ modules/310-ejabberd-accounts/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Création des comptes sur le bus XMPP"
-service ejabberd status || service ejabberd start
+service ejabberd status &> /dev/null  || service ejabberd start
 sleep 2
 for connector in connector-nagios connector-metro connector-vigiboard connector-diode correlator; do
     password=`awk '/^password/ {print $3}' /etc/vigilo/$connector/settings.ini`
