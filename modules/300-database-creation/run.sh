5c204f9255b3e5df6bb383c082e17677dd42dd43
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2010-10-02 08:07:33 +0000

    Ajout de l'accès à la base PGSQL
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@4816 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/300-database-creation/run.sh
+++ modules/300-database-creation/run.sh
@@ -11,6 +11,16 @@ dbpasswd=`awk -F= '/^sqlalchemy_url/ {gsub("\\\\w+://vigilo:","",$2); gsub("@.*"
 echo "Base configurée: $dbname. Utilisateur configuré: $dbuser."
 sleep 5
 
+
+# Autoriser les connexions de l'utilisateur Vigilo
+if grep -qs 'local \+all \+all \+ident \+sameuser' /var/lib/pgsql/data/pg_hba.conf; then
+    if ! grep -qs vigilo /var/lib/pgsql/data/pg_hba.conf; then
+        echo "Attention, l'utilisateur vigilo n'est pas autorisé à se connecter à la base de données. Je l'ajoute, mais il faudra vérifier les permissions dans /var/lib/pgsql/data/pg_hba.conf" | fmt
+        sed -i -e '/^# TYPE\s\+DATABASE\s\+USER\s\+CIDR-ADDRESS\s\+METHOD\s*$/a host vigilo vigilo 127.0.0.1/32 md5' /var/lib/pgsql/data/pg_hba.conf
+    fi
+fi
+
+
 # Utilisateur
 psql -U postgres -A -t -c '\du' | grep -qs '^'$dbuser || \
     psql -U postgres -c "CREATE ROLE $dbuser PASSWORD '$dbpasswd' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;"
