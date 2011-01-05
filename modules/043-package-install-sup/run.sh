41761e30e56f5256b20c8f8b450647642625d353
Grégory CHARBONNEAU <gregory.charbonneau@c-s.fr>
2011-01-05 14:08:02 +0000

    Prise en compte du collecteur Telnet lors de l'installation.
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@5657 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/043-package-install-sup/run.sh
+++ modules/043-package-install-sup/run.sh
@@ -4,6 +4,7 @@ echo "Installation des paquets Vigilo pour un serveur de collecte"
 pkgs="
 vigilo-collector
 vigilo-collector-enterprise
+vigilo-collector-telnet
 vigilo-connector-nagios
 vigilo-perfdata2vigilo
 vigilo-vigiconf-local
