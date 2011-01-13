6ab56c5b098349fb84041c286d83b2a9397c7e2f
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2011-01-13 15:14:05 +0000

    suite r5599
    git-svn-id: https://vigilo-dev.si.c-s.fr/svn@5752 b22e2e97-25c9-44ff-b637-2e5ceca36478

--- modules/033-base-dependencies-sup/run.sh
+++ modules/033-base-dependencies-sup/run.sh
@@ -9,3 +9,9 @@ fi
 
 pkgs=`grep -v '^[[:space:]]*#' $pkglist | grep -v '^[[:space:]]*$'`
 $PKG_INSTALLER $pkgs
+
+
+if [ "$DISTRO" == "mandriva"  ] ; then
+    # on désactive le configuration de la commande de check_dummy (on la définit dans vigilo.cfg)
+    mv /etc/nagios/plugins.d/check_dummy.cfg /etc/nagios/plugins.d/check_dummy.cfg.old
+fi
