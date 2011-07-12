6d03dcbe206150392b1941c49b33f26097bf51f8
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2011-07-12 17:22:12 +0200

    Amélioration de la conf memcached (cf [sha:9048856])
    Change-Id: I80de6d0720744ec9f9ab78a8add9d117fe93a770

--- modules/500-start-memcached/run.sh
+++ modules/500-start-memcached/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Démarrage par supervisor
+cp -a memcached.ini /etc/supervisord.d/
 chkconfig memcached off
 chkconfig supervisord on
 service supervisord status >/dev/null 2>&1 || service supervisord start
