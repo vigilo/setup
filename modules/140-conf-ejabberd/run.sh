648a9dccf98606ebc863383eaffa092457a5945f
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-01-04 14:59:27 +0100

    Debut d'adaptation du setup
    Change-Id: I60061a507218b01512c5515fec2f18e5c1f043aa

--- modules/140-conf-ejabberd/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ! -f /etc/ejabberd/ejabberd.cfg.orig ]; then
-    echo "Patch /etc/ejabberd/ejabberd.cfg"
-    cfgpatch=ejabberd.$DISTRO.patch
-    patch -N -b /etc/ejabberd/ejabberd.cfg < $cfgpatch
-    chown root:ejabberd /etc/ejabberd/ejabberd.cfg
-fi
---
+++ modules/310-rabbitmq-accounts/run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+[ -f /etc/init.d/rabbitmq-server ] || exit 0
+
+echo "Création des comptes sur le bus"
+chkconfig rabbitmq-server on
+service rabbitmq-server status &> /dev/null || service rabbitmq-server start || exit $?
+sleep 5
+rabbitmqctl add-user vigilo-admin vigilo-admin || :
+
+CONNECTORS="connector-diode connector-nagios connector-metro connector-syncevents connector-script correlator vigiconf vigiconf-hls"
+
+for connector in $CONNECTORS; do
+    settings_files=`fgrep -l /etc/vigilo/$connector/*.ini`
+    if [ -z "$settings_files" ]; then
+        rabbitmqctl add-user $connector $connector || :
+    else
+        for settings_file in $settings_files; do
+            username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
+            password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
+            rabbitmqctl add-user $username $password || :
+        done
+    fi
+done
