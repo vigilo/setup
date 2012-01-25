563610c27c7733435a86be4a84111074ddd4f9b5
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-01-25 19:01:38 +0100

    Correction du setup
    Change-Id: Ibbbb378d3e7e4f82c795316386bce09a84e5b722

--- modules/310-rabbitmq-accounts/run.sh
+++ modules/310-rabbitmq-accounts/run.sh
@@ -6,19 +6,19 @@ echo "Création des comptes sur le bus"
 chkconfig rabbitmq-server on
 service rabbitmq-server status &> /dev/null || service rabbitmq-server start || exit $?
 sleep 5
-rabbitmqctl add-user vigilo-admin vigilo-admin || :
+rabbitmqctl add_user vigilo-admin vigilo-admin || :
 
 CONNECTORS="connector-diode connector-nagios connector-metro connector-syncevents connector-script correlator vigiconf vigiconf-hls"
 
 for connector in $CONNECTORS; do
-    settings_files=`fgrep -l /etc/vigilo/$connector/*.ini`
+    settings_files=`fgrep -l '[bus]' /etc/vigilo/$connector/*.ini`
     if [ -z "$settings_files" ]; then
-        rabbitmqctl add-user $connector $connector || :
+        rabbitmqctl add_user $connector $connector || :
     else
         for settings_file in $settings_files; do
             username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
             password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
-            rabbitmqctl add-user $username $password || :
+            rabbitmqctl add_user $username $password || :
         done
     fi
 done
