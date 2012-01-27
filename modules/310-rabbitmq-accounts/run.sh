9b596714d7e3306b0f91cca3e92058ec5ac1cb7a
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-01-27 14:54:11 +0100

    Corrections à l'install rabbitmq
    Change-Id: I96bc104eca08bd83f07b3d1c4ae84879f0640676

--- modules/310-rabbitmq-accounts/run.sh
+++ modules/310-rabbitmq-accounts/run.sh
@@ -7,18 +7,21 @@ chkconfig rabbitmq-server on
 service rabbitmq-server status &> /dev/null || service rabbitmq-server start || exit $?
 sleep 5
 rabbitmqctl add_user vigilo-admin vigilo-admin || :
+rabbitmqctl set_permissions vigilo-admin '.*' '.*' '.*'
 
 CONNECTORS="connector-diode connector-nagios connector-metro connector-syncevents connector-script correlator vigiconf vigiconf-hls"
 
 for connector in $CONNECTORS; do
-    settings_files=`fgrep -l '[bus]' /etc/vigilo/$connector/*.ini`
+    settings_files=`fgrep -l '[bus]' /etc/vigilo/$connector/*.ini 2>/dev/null`
     if [ -z "$settings_files" ]; then
         rabbitmqctl add_user $connector $connector || :
+        rabbitmqctl set_permissions $connector 'queue' '.*' '.*'
     else
         for settings_file in $settings_files; do
             username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
             password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
             rabbitmqctl add_user $username $password || :
+            rabbitmqctl set_permissions $username 'queue' '.*' '.*'
         done
     fi
 done
--- modules/320-rabbitmq-items/run.sh
+++ modules/320-rabbitmq-items/run.sh
@@ -2,20 +2,4 @@
 
 echo "Création des exchanges sur le bus AMQP"
 
-nodes=(
-    event
-    correlation
-    perf
-    state
-    nagios
-    command
-    statistics
-    computation-order
-)
-
-for i in `seq 1 ${#nodes[@]}`; do
-    echo -n ${nodes[$i-1]}' '
-    # @TODO: Le mot de passe (-p) devrait être personnalisable
-    # et ne devrait pas apparaître sur la ligne de commandes.
-    vigilo-bus-config -u vigilo-admin@localhost -p vigilo-admin create-exchange ${nodes[$i-1]}
-done
+vigilo-bus-config -u vigilo-admin -p vigilo-admin read-config exchanges.ini
