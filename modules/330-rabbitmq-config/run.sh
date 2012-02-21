f6dc52cb79e6dd02344c0153b928f6b1176fc544
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-02-21 16:53:05 +0100

    Gestion du SSL sur le bus
    Change-Id: I27667e18aee47cbb8ef336467b37c0320f52c942

---
+++ modules/330-rabbitmq-config/run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Management
+echo "Activation de l'interface de gestion"
+rabbitmq-plugins enable rabbitmq_management
+
+# SSL
+sslcert=/etc/pki/tls/certs/rabbitmq.pem
+sslkey=/etc/pki/tls/private/rabbitmq.pem
+if [ ! -f $sslcert ]; then
+    echo "Generating SSL certificate for RabbitMQ..."
+    HOSTNAME=$(hostname -s 2>/dev/null || echo "localhost")
+    DOMAINNAME=$(hostname -d 2>/dev/null || echo "localdomain")
+    openssl genrsa -out $sslkey 2048 > /dev/null 2>&1
+    openssl req -new -x509 -days 3650 -nodes -out $sslcert \
+                -key $sslkey > /dev/null 2>&1 <<+++
+.
+.
+.
+$DOMAINNAME
+$HOSTNAME
+rabbitmq
+root@$HOSTNAME.$DOMAINNAME
++++
+chown rabbitmq:rabbitmq $sslcert $sslkey
+chmod 600 $sslkey
+fi
+
+# Fichier de config
+cp -pu rabbitmq.config /etc/rabbitmq/
+
+# Redémarrage
+/etc/init.d/rabbitmq-server restart
--- modules/330-rabbitmq-management/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "Activation de l'interface de gestion"
-rabbitmq-plugins enable rabbitmq_management
-/etc/init.d/rabbitmq-server restart
