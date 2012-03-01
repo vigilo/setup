ed0b003fba964dd6b316e233aa7ac82aa771218d
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-03-01 12:55:10 +0100

    Correction des permissions AMQP à l'install
    Change-Id: I956c64538bda8e75a1b3efec576e842d7b7793ac

--- modules/310-rabbitmq-accounts/run.sh
+++ modules/310-rabbitmq-accounts/run.sh
@@ -15,13 +15,13 @@ for connector in $CONNECTORS; do
     settings_files=`fgrep -l '[bus]' /etc/vigilo/$connector/*.ini 2>/dev/null`
     if [ -z "$settings_files" ]; then
         rabbitmqctl add_user $connector $connector || :
-        rabbitmqctl set_permissions $connector 'queue' '.*' '.*'
+        rabbitmqctl set_permissions $connector '.*' '.*' '.*'
     else
         for settings_file in $settings_files; do
             username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
             password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
             rabbitmqctl add_user $username $password || :
-            rabbitmqctl set_permissions $username 'queue' '.*' '.*'
+            rabbitmqctl set_permissions $username '.*' '.*' '.*'
         done
     fi
 done
