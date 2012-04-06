50918dae710eedd7a5677ed34d924e717b67cf3a
Aurelien BOMPARD <aurelien.bompard@c-s.fr>
2012-04-06 11:42:48 +0200

    Ajout et mises à jour de copyrights, complète le [sha:3c99844]
    Change-Id: I47c53310f710006097927c666387207c1f4125aa
    Fixup: 3c99844c7630b66e3f267cc08d169a5c91b73572
    BuildBot: no-build

--- modules/500-start-memcached/run.sh
+++ modules/500-start-memcached/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Copyright (C) 2011-2012 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 # Démarrage par supervisor
 cp -a memcached.ini /etc/supervisord.d/
--- modules/520-start-nrpe/run.sh
+++ modules/520-start-nrpe/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Copyright (C) 2011-2012 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 chkconfig nrpe on
 service nrpe status &> /dev/null
