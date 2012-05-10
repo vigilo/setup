389d6fc7045a4edcf516985574a92b2bc018d947
Thomas BURGUIERE <thomas.burguiere@c-s.fr>
2012-05-10 11:47:13 +0200

    Création user bus vigiconf-hls connector-vigiconf
    L'utilisateur pour vigiconf-hls était créé deux fois.
    L'utilisateur pour connector-vigiconf n'était pas créé.
    
    Change-Id: Iac90457aa111253476c9b6ac69a6822a8d9de9c9
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/705
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Aurelien BOMPARD <aurelien.bompard@c-s.fr>

--- modules/320-rabbitmq-accounts/run.sh
+++ modules/320-rabbitmq-accounts/run.sh
@@ -2,6 +2,24 @@
 # Copyright (C) 2011-2012 CS-SI
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
+function creation_BUS_user {
+    connector=$1
+    shift
+    settings_files="$@"
+
+    if [ -z "$settings_files" ]; then
+        rabbitmqctl add_user $connector $connector || :
+        rabbitmqctl set_permissions $connector '.*' '.*' '.*'
+    else
+        for settings_file in $settings_files; do
+            username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
+            password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
+            rabbitmqctl add_user $username $password || :
+            rabbitmqctl set_permissions $username '.*' '.*' '.*'
+        done
+    fi
+}
+
 [ -f /etc/init.d/rabbitmq-server ] || exit 0
 
 echo "Création des comptes sur le bus"
@@ -15,15 +33,5 @@ CONNECTORS="connector-nagios connector-metro correlator"
 
 for connector in $CONNECTORS; do
     settings_files=`fgrep -l '[bus]' /etc/vigilo/$connector/*.ini 2>/dev/null`
-    if [ -z "$settings_files" ]; then
-        rabbitmqctl add_user $connector $connector || :
-        rabbitmqctl set_permissions $connector '.*' '.*' '.*'
-    else
-        for settings_file in $settings_files; do
-            username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
-            password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
-            rabbitmqctl add_user $username $password || :
-            rabbitmqctl set_permissions $username '.*' '.*' '.*'
-        done
-    fi
+    creation_BUS_user $connector $settings_files
 done
