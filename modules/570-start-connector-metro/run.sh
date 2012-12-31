dbe3c37ee9359aa82b07bbe6dd9420fdfc925799
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-12-31 13:07:10 +0100

    Suppression des shebangs.
    Évite toute confusion par rapport au shell à utiliser et évite que les
    modules soient invoquées directement (manuellement).
    
    Change-Id: I2060428ff03ca0c18b80bddc0825c50e9cf6087d
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1069
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

--- modules/570-start-connector-metro/run.sh
+++ modules/570-start-connector-metro/run.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+# Copyright (C) 2011-2012 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 # Import de la couche de compatibilité.
 . "`dirname $0`/../compat.sh"
