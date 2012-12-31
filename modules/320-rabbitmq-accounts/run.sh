a50fd70c40f9863ce7b52ce474f42cf6297b23dd
Francois POIROTTE <francois.poirotte@c-s.fr>
2012-12-31 16:37:14 +0100

    Renommage de creation_BUS_user et mise en commun.
    Renomme la fonction "creation_BUS_user" en "create_bus_user" et mise en
    commun via le fichier compat.sh (afin de partager la fonction entre la
    VCom et la VEnt).
    
    Change-Id: I9fbefc9c08b000c17722601fb9d0244ed73d7b84
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1071
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>
    Reviewed-by: Francois POIROTTE <francois.poirotte@c-s.fr>

--- modules/320-rabbitmq-accounts/run.sh
+++ modules/320-rabbitmq-accounts/run.sh
@@ -5,24 +5,6 @@
 # Import de la couche de compatibilité.
 . "`dirname $0`/../compat.sh"
 
-function creation_BUS_user {
-    connector=$1
-    shift
-    settings_files="$@"
-
-    if [ -z "$settings_files" ]; then
-        rabbitmqctl add_user $connector $connector || :
-        rabbitmqctl set_permissions $connector '.*' '.*' '.*'
-    else
-        for settings_file in $settings_files; do
-            username=`vigilo-config -s bus -g user $settings_file 2>/dev/null || :`
-            password=`vigilo-config -s bus -g password $settings_file 2>/dev/null || :`
-            rabbitmqctl add_user $username $password || :
-            rabbitmqctl set_permissions $username '.*' '.*' '.*'
-        done
-    fi
-}
-
 [ -f /etc/init.d/rabbitmq-server ] || exit 0
 
 echo "Création des comptes sur le bus"
@@ -34,5 +16,5 @@ CONNECTORS="connector-nagios connector-metro correlator"
 
 for connector in $CONNECTORS; do
     settings_files=`fgrep -l '[bus]' /etc/vigilo/$connector/*.ini 2>/dev/null`
-    creation_BUS_user $connector $settings_files
+    create_bus_user $connector $settings_files
 done
