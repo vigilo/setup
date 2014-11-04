106cc6d31b91e844be461e0b10fb3aeb7e2d859a
Francois POIROTTE <francois.poirotte@c-s.fr>
2014-11-04 15:54:49 +0100

    [#1301] Eviter le mode compatibilité IE7.
    Force les IHMs à s'afficher avec le dernier moteur de rendu disponible
    sous IE. Ceci est nécessaire pour éviter certains problèmes d'affichage
    ou de comportement lorsque IE décide de passer en mode "compatibilité".
    Voir aussi #1266 sur ce dernier sujet.
    
    Change-Id: Id6d0be8c2e57a888e6da16d7fab25d5165f5a2b1
    Refs: #1301.
    Reviewed-on: https://vigilo-dev.si.c-s.fr/review/1519
    Tested-by: Build system <qa@vigilo-dev.si.c-s.fr>
    Reviewed-by: Thomas BURGUIERE <thomas.burguiere@c-s.fr>

---
+++ modules/220-conf-apache/run.sh
@@ -0,0 +1,14 @@
+# Copyright (C) 2006-2014 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+# Import de la couche de compatibilité.
+. "`dirname $0`/../compat.sh"
+
+echo "Configuration du serveurs web Apache"
+if [ "$DISTRO" == "debian" ]; then
+    a2enmod headers
+fi
+
+# On ne redémarre pas Apache, le script
+# 550-restart-apache le fera pour nous.
+
