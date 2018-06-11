3f15d986f4b4b888e30db1bd4837af584761d78d
Francois POIROTTE <francois.poirotte@c-s.fr>
2018-06-11 17:14:55 +0200

    [#1742] Refuser de s'installer avec SELinux
    L'installation de Vigilo sur une machine où SELinux est activé pose
    problème (notamment pour le démarrage de rabbitmq-server).
    
    En attendant de disposer de règles SELinux adéquates (cf. #1633),
    on refuse de démarrer l'installation si SELinux est installé
    et s'exécute en mode "Enforcing".
    
    Change-Id: I23585c01279905168facc7af60363f88c0dd2b62
    Refs: #1742

---
+++ modules/010-conf-selinux/run.sh
@@ -0,0 +1,15 @@
+# Copyright (C) 2018 CS-SI
+# License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
+
+# Import de la couche de compatibilité.
+. "`dirname $0`/../compat.sh"
+
+echo "Vérification de la configuration de SELinux"
+
+status=`getenforce 2> /dev/null`
+if [ "$status" = "Enforcing" ]; then
+    echo "Vigilo ne peut pas s'exécuter lorsque SELinux est activé." >&2
+    echo "Veuillez désactiver SELinux ou le configurer en mode 'Permissive' " >&2
+    echo "avant de continuer." >&2
+    exit 1
+fi
