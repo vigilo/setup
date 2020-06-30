fe22d2bce66d39c2ba2e56b8963d5b7d2cbbcb77
Francois POIROTTE <francois.poirotte@c-s.fr>
2020-06-30 11:28:57 +0200

    [#2026] Changement de dénomination sociale
    Change-Id: Ic65534c55dbce2128e20cc44e70b0b1618c69d37
    Refs: #2026

--- modules/550-restart-apache/run.sh
+++ modules/550-restart-apache/run.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2020 CS-SI
+# Copyright (C) 2011-2020 CS GROUP – France
 # License: GNU GPL v2 <http://www.gnu.org/licenses/gpl-2.0.html>
 
 # Import de la couche de compatibilité.
